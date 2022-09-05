f18eb22dbd3ffa227785b35bc2126bc1f5d5351f
Nagy Zoltan <zoltan.nagy@port.hu>
2022-09-05 02:33:58 +0200

    not succ and valid url tests

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -8,6 +8,19 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class PageController extends AbstractController
 {
+    #[Route(
+        '/{handle1}/{handle2}/{method}',
+        name: 'app_index',
+        condition: "params['handle1'] !== params['handle2'] && params['method'] in ['mod', 'fib']"
+    )]
+    public function index(string $handle1, string $handle2, string $method = 'fib'): Response
+    {
+        return $this->render('page/index.html.twig', [
+            'message' => 'Index',
+            'params' => "{$handle1} / {$handle2} / {$method}",
+        ]);
+    }
+
     #[Route('/hello', name: 'app_hello')]
     public function hello(): Response
     {
--- tests/Controller/PageControllerTest.php
+++ tests/Controller/PageControllerTest.php
@@ -3,15 +3,65 @@
 namespace App\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class PageControllerTest extends WebTestCase
 {
-    public function testHelloPage(): void
+    public function test_hello_page(): void
     {
         $client = static::createClient();
         $crawler = $client->request('GET', '/hello');
 
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h1', 'Hello World');
+    }
+
+    /**
+     * @dataProvider sameHandleParametersUnsuccessfulUrlProvider
+     */
+    public function test_same_handle_parameters_unsuccessful($url): void
+    {
+        $client = static::createClient();
+
+        $client->catchExceptions(false);
+        $this->expectException(NotFoundHttpException::class);
+
+        $crawler = $client->request('GET', $url);
+    }
+
+    public function test_mod_parameters_unsuccessful(): void
+    {
+        $client = static::createClient();
+
+        $client->catchExceptions(false);
+        $this->expectException(NotFoundHttpException::class);
+
+        $crawler = $client->request('GET', '/knplabs/symfony/dog');
+    }
+
+    /**
+     * @dataProvider successfulUrlProvider
+     */
+    public function test_successful_pages($url): void
+    {
+        $client = self::createClient();
+        $client->request('GET', $url);
+
+        $this->assertResponseIsSuccessful();
+
+    }
+
+    public function sameHandleParametersUnsuccessfulUrlProvider()
+    {
+        yield ['/symfony/symfony'];
+        yield ['/knplabs/knplabs/mod'];
+        yield ['/symfony/symfony/fib'];
+    }
+
+    public function successfulUrlProvider()
+    {
+        yield ['/knplabs/symfony/mod'];
+        yield ['/symfony/knplabs/fib'];
+        yield ['/potus/github'];
     }
 }
